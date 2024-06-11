1b6bb29b856fa34f182dac8f1e2c04970aae97a3
Anthony Leung <anthonyleung07@gmail.com>
2024-06-11 15:19:20 +0100

    Setup and testing
    AWS S3 Cloudfront setup with Vite + React (added Tailwind but using default react files for setup, will change format in the future)
    used Elemental MediaConvert to convert the video to 1080p and 30Hz MP4 setup
    
    video source - a random gameplay snipet of red dead redemption 2 that I did :))

---
+++ src/App.tsx
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+    const [count, setCount] = useState(0)
+
+    return (
+        <>
+            <div>
+                <a href="https://vitejs.dev" target="_blank">
+                    <img src={viteLogo} className="logo" alt="Vite logo" />
+                </a>
+                <a href="https://react.dev" target="_blank">
+                    <img src={reactLogo} className="logo react" alt="React logo" />
+                </a>
+            </div>
+            <h1>Vite + React</h1>
+            <div className="card">
+                <button onClick={() => setCount((count) => count + 1)}>
+                    count is {count}
+                </button>
+                <p>
+                    Edit <code>src/App.tsx</code> and save to test HMR
+                </p>
+            </div>
+            <p className="read-the-docs">
+                Click on the Vite and React logos to learn more
+            </p>
+            <video width={1280} height={500} controls loop autoPlay muted>
+                <source src="https://d1o1qq3rynw355.cloudfront.net/rdr2_gameplay-1080p30Hz.mp4" type="video/mp4" />
+            </video>
+        </>
+    )
+}
+
+export default App
