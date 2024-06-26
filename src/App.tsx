6f702a154e40f1e61f5881592fc927fd7ed88ac3
Anthony Leung <anthonyleung07@gmail.com>
2024-06-26 15:15:49 +0100

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
-function App() {
-    const [count, setCount] = useState(0)
+import AppLayout from '@components/AppLayout'
 
+import Home from '@routes/Home.tsx'
+
+const router = createBrowserRouter([{
+    path: "/",
+    element: <AppLayout />,
+    children: [
+        {
+            path: "/",
+            element: <Home />,
+        },
+    ]
+}]);
+
+const App: React.FC = (): JSX.Element => {
     return (
         <>
-            <div>
-                <a href="https://vitejs.dev" target="_blank">
-                    <img src={viteLogo} className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://react.dev" target="_blank">
-                    <img src={reactLogo} className="logo react" alt="React logo" />
-                </a>
-            </div>
-            <h1>Vite + React</h1>
-            <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>
-                    count is {count}
-                </button>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to test HMR
-                </p>
-            </div>
-            <p className="read-the-docs">
-                Click on the Vite and React logos to learn more
-            </p>
-            <video width={1280} height={500} controls loop autoPlay muted>
-                <source src="https://d1o1qq3rynw355.cloudfront.net/rdr2_gameplay-1080p30Hz.mp4" type="video/mp4" />
-            </video>
+            <RouterProvider router={router} />
         </>
-    )
-}
+    );
+};
 
-export default App
+export default App;
